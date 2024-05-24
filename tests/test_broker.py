9bdf34a0e37a64c36c124708bcbab0bde9af3c5f
Florian Schweikert <florian.schweikert@husqvarnagroup.com>
2024-05-24 12:12:35 +0200

    tests: allow 200ms to establish connection in test

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -119,7 +119,7 @@ async def test_connect_tcp(broker):
         if conn.status == "ESTABLISHED":
             open_connections.append(conn)
     assert len(open_connections) == 1
-    await asyncio.sleep(0.1)
+    await asyncio.sleep(0.2)
     assert broker._servers["default"].conn_count == 1
 
 
